fa3ada5636e55a74d2e080f11f7d52dbad356d50
drew58 <Andrewenahoro2019gmail.com>
2023-07-31 09:40:32 +0100

    https://github.com/drew58/my-portfoliopo.git

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,17 +1,12 @@
 import React from 'react';
+import ContactForm from '../components/ContactForm'; // Adjust the path based on your folder structure
 
 const Contact = () => {
   return (
     <section id="contact" className="section">
       <div className="container">
         <h2>Contact Me</h2>
-        <form name="contact" method="POST" netlify>
-        <input type="text" name="name" placeholder="Your Name" required />
-          <input type="email" name="email" placeholder="Your Email" required />
-          <textarea name="message" placeholder="Your Message" required></textarea>
-          <button type="submit">Submit</button>
-        </form>
-
+        <ContactForm />
       </div>
     </section>
   );
